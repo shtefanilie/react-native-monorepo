0293c1f866b8d073086ae43919f8e1f491149308
Stefan Ilie <ilie.stefan92@yahoo.com>
2022-07-03 22:23:08 +0100

    [viplm] first commit

---
+++ apps/vremeainpulamea/pages/_document.js
@@ -0,0 +1,2 @@
+// @generated: @expo/next-adapter@4.0.7
+export { default } from '@expo/next-adapter/document';
---
+++ apps/vremeainpulamea/pages/index.js
@@ -0,0 +1,22 @@
+// @generated: @expo/next-adapter@4.0.7
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Welcome to Expo + Next.js 👋</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: 16,
+  },
+});
